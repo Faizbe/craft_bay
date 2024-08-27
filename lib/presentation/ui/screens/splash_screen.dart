38746e357b06d7dc45a5a63528a12117213586ea
Faizbe <ashiqulfaizbe7874@gmail.com>
2024-08-27 11:48:00 +0600

    updated main, app and splash screen

--- lib/presentation/ui/screens/splash_screen.dart
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:craft_bay/presentation/ui/screens/verify_email_screen.dart';
+import 'package:craft_bay/presentation/ui/widgets/app_logo.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,10 +12,38 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    moveToNextScreen();
+  }
+
+  void moveToNextScreen() async {
+    await Future.delayed(
+      const Duration(seconds: 1),
+    );
+    // await Get.find<AuthController>().initialize();
+    Get.offAll(const VerifyEmailScreen());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: CircularProgressIndicator(),
+        child: Column(
+          children: [
+            Spacer(),
+            AppLogo(),
+            Spacer(),
+            CircularProgressIndicator(),
+            SizedBox(
+              height: 16,
+            ),
+            Text('Version 1.0'),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/presentation/ui/screens/verify_email_screen.dart
@@ -0,0 +1,95 @@
+import 'package:craft_bay/presentation/ui/widgets/app_logo.dart';
+import 'package:flutter/material.dart';
+
+class VerifyEmailScreen extends StatefulWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  State<VerifyEmailScreen> createState() => _VerifyEmailScreenState();
+}
+
+class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 160,
+                ),
+                const AppLogo(
+                  height: 80,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  'Welcome back',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Please enter your email address',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  controller: _emailTEController,
+                  decoration: const InputDecoration(hintText: 'Email'),
+                  validator: (value) {
+                    if (value?.trim().isEmpty ?? true) {
+                      return 'Enter your email';
+                    }
+                    // TODO: Validate email with Regex
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: ()  {
+                      // if (_formKey.currentState!.validate()) {
+                      //   final bool result = await controller
+                      //       .sendOtpToEmail(_emailTEController.text.trim());
+                      //   if (result) {
+                      //     Get.to(
+                      //       () => VerifyOTPScreen(
+                      //         email: _emailTEController.text.trim(),
+                      //       ),
+                      //     );
+                      //   } else {
+                      //     Get.showSnackbar(GetSnackBar(
+                      //       title: 'Send OTP failed',
+                      //       message: controller.errorMessage,
+                      //       duration: const Duration(seconds: 2),
+                      //       isDismissible: true,
+                      //     ));
+                      //   }
+                      // }
+                    },
+                    child: const Text('Next'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
